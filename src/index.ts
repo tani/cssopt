67600c3b5945341c44182efb5f86baa3940ecb71
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-12-06 12:29:39 +0000

    rename to hast-util-inline-css

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
-import { fromParse5 } from "hast-util-from-parse5"
-import { selectAll } from "hast-util-select"
-import parse5 from "parse5"
+import { selectAll, HastNode } from "hast-util-select"
+import { toString } from "hast-util-to-string";
 import css from "css"
+import { remove } from "unist-util-remove"
 
-function inline(html: string, stylesheet: string) {
-    const past = parse5.parseFragment(html)
-    const hast = fromParse5(past)
+export function inline(hast: HastNode): HastNode | null {
+    const stylesheet = selectAll('style', hast).map(toString).join("\n")
     const cast = css.parse(stylesheet)
-    for (const rule of cast.stylesheet.rules) {
-        rule.type == ""
+    for (const rule of cast.stylesheet?.rules ?? []) {
+        if ("selectors" in rule) {
+            for(const selector of rule.selectors ?? []) {
+                const elems = selectAll(selector, hast)
+                for(const elem of elems) {
+                    for(const declaration of rule.declarations ?? []) {
+                        if("property" in declaration) {
+                            elem.properties ??= {}
+                            elem.properties.style ??= ""
+                            if(/[^;]\s*$/.test(elem.properties.style as string)) {
+                                elem.properties.style += ";"
+                            }
+                            elem.properties.style += `${declaration.property}:${declaration.value};`
+                        }
+                    }
+                }
+            }
+        }
     }
-    const elems = selectAll('', hast)
+    
+    return remove(hast, { tagName: "style" })
 }
